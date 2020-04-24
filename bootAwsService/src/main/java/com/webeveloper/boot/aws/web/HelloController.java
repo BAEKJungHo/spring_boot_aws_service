fc6e50f59f07ad5ed796d651a53304b37fb3a9dc
BAEKJungHo <designjava@naver.com>
2020-04-24 21:09:52 +0900

    Junit 을 이용한 API 테스트 - ResultMatcher - hamcrest.Matchers.is

--- bootAwsService/src/main/java/com/webeveloper/boot/aws/web/HelloController.java
+++ bootAwsService/src/main/java/com/webeveloper/boot/aws/web/HelloController.java
@@ -1,6 +1,9 @@
 package com.webeveloper.boot.aws.web;
 
+import com.webeveloper.boot.aws.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,15 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @discription HelloController
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     /**
      * @RestController : JSON 을 반환하는 컨트롤러로 만들어준다.
      * @return
      */
-    @GetMapping("/hello")
+    @GetMapping
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount
+    ) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
--- bootAwsService/src/test/java/com/webeveloper/boot/aws/web/HelloControllerTest.java
+++ bootAwsService/src/test/java/com/webeveloper/boot/aws/web/HelloControllerTest.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -24,6 +25,27 @@ public class HelloControllerTest {
         mockMvc.perform(get("/hello")) // hello 주소로 HTTP GET 요청
                     .andExpect(status().isOk()) // mockMvc.perform 결과의 상태 코드가 200인지
                     .andExpect(content().string(hello)); // 응답 본문의 내용이 hello 인지
+    }
+
+    @Test
+    public void helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        /**
+         * jsonPath
+         * - json 응답값을 필드별로 검증할 수 있는 메서드
+         * - 여기서 return 값은 ResultMatcher
+         * is
+         * - import static org.hamcrest.Matchers.is;
+         */
+        mockMvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 
 }
